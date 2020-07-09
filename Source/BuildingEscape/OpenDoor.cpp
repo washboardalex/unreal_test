1ca6d047f74a8436380a4d131b1a269cb1d2aa4e
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-07-09 18:52:29 +1000

    very basic level with some 101 scripting

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// MIT Licence yo.
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    InitialYaw = GetOwner()->GetActorRotation().Yaw;
+    CurrentYaw = InitialYaw;
+	TargetYaw += InitialYaw;
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+    UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+    CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 1.5f);
+    FRotator DoorRotation = GetOwner()->GetActorRotation();
+    DoorRotation.Yaw=CurrentYaw;
+    GetOwner()->SetActorRotation(DoorRotation);
+}
+
